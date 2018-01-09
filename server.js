af3ff09573e27c6ef844800ec26bfc2867e40fe7
Daniel <daniel_defante@hotmail.com>
2018-01-08 22:57:44 -0200

    mudando versao e message porta

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.set('port', port);
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log(`Servidor rodando na porta: ${port}`);
+    console.log(`Servidor Rodando na Porta: ${port}`);
 });
